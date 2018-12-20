8336ee6fdfa1389e8ffe2af4e3bcb10df4064f5a
Chris Tarazi <ctarazi@pivotal.io>
2018-12-20 13:53:44 -0800

    Refactor create-org task
    * replace inline task with task file
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- ci/tasks/create-org/task.sh
+++ ci/tasks/create-org/task.sh
@@ -2,11 +2,11 @@
 
 set -eux
 
-my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ci_dir="${my_dir}/../../"
-workspace_dir="${ci_dir}/../"
+workspace_dir=$(pwd)
+ci_dir="${workspace_dir}/cf-mysql-ci/"
 
 source ${ci_dir}/scripts/utils.sh
+
 credhub_login
 
 api_url="api.$(cf_domain)"
