882d42c9e82434611c3656fa423ea9746ec8d69c
Kai Xiang <kxiang@pivotal.io>
2019-01-16 15:05:34 -0800

    Refactor scaling-arb-to-full-test
    * Use parameterized test-scaling task to replace scaling-arb-to-full-test
    
    Signed-off-by: James Palmer <jpalmer@pivotal.io>

--- ci/tasks/test-scaling/task.sh
+++ ci/tasks/test-scaling/task.sh
@@ -5,9 +5,9 @@ set -eux
 tmpdir=$(mktemp -d /tmp/mysql_scaling.XXXXX)
 trap '{ rm -rf ${tmpdir}; }' EXIT
 
-MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CI_DIR="$( cd "${MY_DIR}/../../../" && pwd )"
-WORKSPACE_DIR="$( cd "${MY_DIR}/../../../../" && pwd )"
+WORKSPACE_DIR="$(pwd)"
+CI_DIR="${WORKSPACE_DIR}/cf-mysql-ci"
+MY_DIR="${CI_DIR}/ci/tasks/test-scaling"
 
 : "${ENV_TARGET_FILE:?}"
 : "${BOSH_DEPLOYMENT:?}"
@@ -19,17 +19,29 @@ export BOSH_CA_CERT="${WORKSPACE_DIR}/bosh-lite-info/ca-cert"
 
 source "${MY_DIR}/scaling-helpers-ondemand"
 
-setup_infrastructure_variables 10.244.7.2 10.244.8.2
+if [[ $SCALE_TO_FULL_TEST == "false" ]]; then
+  setup_infrastructure_variables 10.244.7.2 10.244.8.2
+else
+  setup_infrastructure_variables 10.244.7.2 10.244.8.2 10.244.9.3
+fi
 
 pushd "${MY_DIR}"
   bundle install
 popd
 
-scale_down_to_minimal
+if [[ $SCALE_TO_FULL_TEST == "false" ]]; then
+  scale_down_to_minimal
+fi
+
 clear_data
 write_some_data 1000
 
-scale_up_to_default
+if [[ $SCALE_TO_FULL_TEST == "false" ]]; then
+  scale_up_to_default
+else
+  deploy_full_cluster #this step removes the arbitrator
+fi
+
 verify_replicated_row_count_is 1000
 
 scale_down_to_minimal
