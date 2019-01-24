251890ef70bffaff04ef93e13a28d4c35fa00017
Andrew Garner <agarner@pivotal.io>
2019-01-24 10:22:40 -0600

    Remove WORKSPACE_DIR from fetch-cf-deployment-stemcell task
    - This was causing create-and-compile-release job to fail because of
    relative path mismatch.
    
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- ci/tasks/fetch-cf-deployment-stemcell/task.sh
+++ ci/tasks/fetch-cf-deployment-stemcell/task.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORKSPACE_DIR="$( cd "${MY_DIR}/../../.." && pwd )"
+set -euo pipefail
 
-stemcell_version="$(bosh int "${WORKSPACE_DIR}/cf-deployment-rc/cf-deployment.yml" --path=/stemcells/os=ubuntu-xenial/version)"
-aria2c -x 4 -d ${WORKSPACE_DIR}/cf-deployment-rc-stemcell \
+stemcell_version="$(bosh int "cf-deployment-rc/cf-deployment.yml" --path=/stemcells/os=ubuntu-xenial/version)"
+
+aria2c -x 4 -d cf-deployment-rc-stemcell \
   https://s3.amazonaws.com/bosh-core-stemcells/google/bosh-stemcell-${stemcell_version}-google-kvm-ubuntu-xenial-go_agent.tgz
+
 # the compile release script expects a version file in the stemcell folder
-echo ${stemcell_version} > ${WORKSPACE_DIR}/cf-deployment-rc-stemcell/version
+echo ${stemcell_version} > cf-deployment-rc-stemcell/version
