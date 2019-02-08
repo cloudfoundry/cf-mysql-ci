4ebf22d05e1f76a34195249ce50c54b97632d670
James Palmer <jpalmer@pivotal.io>
2019-02-08 11:22:04 -0800

    Switch run-errand to standard bosh lite source_me
    Signed-off-by: Chris Tarazi <ctarazi@pivotal.io>

--- ci/tasks/run-errand/task.sh
+++ ci/tasks/run-errand/task.sh
@@ -4,6 +4,7 @@ set -eux
 
 WORKSPACE_DIR="$(pwd)"
 ci_repo_dir="${WORKSPACE_DIR}/cf-mysql-ci"
+bosh_lite_info_dir="${WORKSPACE_DIR}/bosh-lite-info"
 
 source "${ci_repo_dir}/scripts/utils.sh"
 
@@ -17,21 +18,7 @@ if [[ ! -z "${INSTANCE}" ]]; then
   options="--instance=${INSTANCE}"
 fi
 
-BOSH_ENVIRONMENT="$(cat "${ENV_TARGET_FILE}")"
-BOSH_ENVIRONMENT="$(sslip_from_ip "${BOSH_ENVIRONMENT}")"
-export BOSH_ENVIRONMENT
-
-BOSH_CLIENT=${BOSH_CLIENT:-"$(jq_val "bosh_user" "${ENV_METADATA}")"}
-export BOSH_CLIENT
-
-BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET:-"$(jq_val "bosh_password" "${ENV_METADATA}")"}
-export BOSH_CLIENT_SECRET
-
-BOSH_CA_CERT=${BOSH_CA_CERT_PATH:-"${ci_repo_dir}/${ENV_REPO}/$(jq_val "env" "${ENV_METADATA}")/certs/rootCA.pem"}
-export BOSH_CA_CERT
-
-BOSH_DEPLOYMENT=${BOSH_DEPLOYMENT:-"${DEPLOYMENT_NAME}"}
-export BOSH_DEPLOYMENT
+source ${bosh_lite_info_dir}/source_me
 
 bosh \
     -n \
