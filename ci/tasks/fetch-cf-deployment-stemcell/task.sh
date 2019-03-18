0f7828874492bcfed007d788c49f98d491ac599b
Stephane Jolicoeur <sjolicoeur@pivotal.io>
2019-03-18 15:04:34 -0700

    Change the script to get the stemcell to fix bosh.io's breaking change in url
    [#164703711]
    
    Signed-off-by: James Palmer <jpalmer@pivotal.io>

--- ci/tasks/fetch-cf-deployment-stemcell/task.sh
+++ ci/tasks/fetch-cf-deployment-stemcell/task.sh
@@ -2,10 +2,20 @@
 
 set -euo pipefail
 
+BREAKING_CHANGE_VERSION="250.16" # This is the version at which the bosh.io site broke the url convention
+
 stemcell_version="$(bosh int "cf-deployment-rc/cf-deployment.yml" --path=/stemcells/os=ubuntu-xenial/version)"
 
-aria2c -x 4 -d cf-deployment-rc-stemcell \
-  https://s3.amazonaws.com/bosh-core-stemcells/google/bosh-stemcell-${stemcell_version}-google-kvm-ubuntu-xenial-go_agent.tgz
+
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+if version_gt $stemcell_version $BREAKING_CHANGE_VERSION; then
+  aria2c -x 4 -d cf-deployment-rc-stemcell \
+    https://s3.amazonaws.com/bosh-core-stemcells/${stemcell_version}/bosh-stemcell-${stemcell_version}-google-kvm-ubuntu-xenial-go_agent.tgz
+else
+  aria2c -x 4 -d cf-deployment-rc-stemcell \
+    https://s3.amazonaws.com/bosh-core-stemcells/google/bosh-stemcell-${stemcell_version}-google-kvm-ubuntu-xenial-go_agent.tgz
+fi
 
 # the compile release script expects a version file in the stemcell folder
 echo ${stemcell_version} > cf-deployment-rc-stemcell/version
