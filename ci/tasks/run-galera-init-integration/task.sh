01de1e500b2a5e63b04eacef5fed19b25937fb8e
Steve Taylor <staylor@pivotal.io>
2019-04-01 16:07:13 -0700

    Integration tests now automagically use docker.
    - We no longer need to start mysqld or create users manually
    - This makes us much happier
    
    [#165028140]
    
    Signed-off-by: Andrew Garner <agarner@pivotal.io>

--- ci/tasks/run-galera-init-integration/task.sh
+++ ci/tasks/run-galera-init-integration/task.sh
@@ -12,29 +12,13 @@ done
 echo "Pulling docker image: percona/percona-xtradb-cluster:5.7"
 time docker pull percona/percona-xtradb-cluster:5.7
 
-cat > /tmp/init.sql <<EOF
-CREATE USER 'root'@'127.0.0.1';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;
-EOF
-
-mysqld --initialize-insecure \
-       --init-file=/tmp/init.sql > mysqld_initialize.log 2>&1
-mysqld --character-set-server=utf8 \
-       --collation-server=utf8_unicode_ci \
-       --daemonize \
-       --log-error=/tmp/mysqld.log
-
 if [[ ! -d /sys/fs/cgroup/systemd ]]; then
-    # Fix for issue where container creation fails: https://github.com/moby/moby/issues/36016
-    mkdir /sys/fs/cgroup/systemd
-    mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
+  # Fix for issue where container creation fails: https://github.com/moby/moby/issues/36016
+  mkdir /sys/fs/cgroup/systemd
+  mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
 fi
 
 pushd pxc-release
-    source .envrc
-    ginkgo -r -slowSpecThreshold=180 \
-           -race \
-           -failOnPending \
-           -randomizeAllSpecs \
-           src/github.com/cloudfoundry/galera-init/integration_test/
+  source .envrc
+  ./scripts/test-integration
 popd
