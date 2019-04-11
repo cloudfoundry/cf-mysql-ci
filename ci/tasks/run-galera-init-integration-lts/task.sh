c0d2659518783f27e978201d91f546d0205a7ce8
Chris Tarazi <ctarazi@pivotal.io>
2019-04-11 11:31:43 -0700

    Clean out default data dir
    [#165229158]
    
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- ci/tasks/run-galera-init-integration-lts/task.sh
+++ ci/tasks/run-galera-init-integration-lts/task.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 trap "pkill -9 mysqld;" EXIT
 
+rm -rf /var/lib/mysql
+mkdir -p /var/lib/mysql
+
 cat > /tmp/init.sql <<EOF
 CREATE USER 'root'@'127.0.0.1';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;
